fe8d7d65788cbac19b6cc61269b5bf25588e3e0f
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2023-03-12 23:26:44 +0530

    fix: minor style update

--- shared/Post.tsx
+++ shared/Post.tsx
@@ -5,13 +5,13 @@ import React from "react";
 const Post = ({ id, title, description, url, created_at }: Post) => {
   return (
     <div>
-      <p className="text-gray-600 ">
+      <p className="text-gray-600 dark:text-gray-400">
         {created_at && new Date(created_at).toDateString()}
       </p>
 
       <a href={url} target="_blank" rel="noreferrer noopener">
         <h3 className="w-full font-bold tracking-tight"> {title} </h3>
-        <p className="text-gray-600 "> {description} </p>
+        <p className="text-gray-600 dark:text-gray-400"> {description} </p>
       </a>
     </div>
   );
